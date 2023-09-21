2b2fecc16cc407c15fc401660e793f28b924bbed
hivernal <geryaevna@mail.ru>
2023-09-21 21:29:37 +0700

    change qemu script

--- .config/scripts/run.sh
+++ .config/scripts/run.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-memory="-m 1G"
-net="-nic user,smb=/home/nikita,mac=52:54:00:12:34:56 -nic bridge,helper=/usr/lib/qemu/qemu-bridge-helper,mac=52:54:00:12:34:57"
-vga="-vga qxl"
-display="-display spice-app,gl=on -device virtio-serial -chardev spicevmc,id=vdagent,debug=0,name=vdagent -device virtserialport,chardev=vdagent,name=com.redhat.spice.0"
-disk="astra4.qcow2"
+SPICE="-display spice-app,gl=on -device virtio-serial -chardev spicevmc,id=vdagent,debug=0,name=vdagent -device virtserialport,chardev=vdagent,name=com.redhat.spice.0"
+TAP="-nic tap,helper=/usr/lib/qemu/qemu-bridge-helper,mac=52:54:00:12:34:56"
+SOCKET="-nic socket,mcast=230.0.0.1:1234,mac=52:54:00:12:34:56"
+USER="-nic user,smb=/home/nikita,mac=52:54:00:12:34:56"
+
+DISPLAY=$SPICE
+VGA="-vga qxl"
+NET=$TAP
+MEM="-m 1G"
+CDROM=
+BOOT=
+DISK="-hda openvpn.qcow2"
 
 while [[ -n $1 ]]; do
   case $1 in
-    -m) memory="-m $2"; shift;;
-    -cdrom) cdrom="-cdrom $2"; shift;;
-    -vga) vga="-vga $2"; shift;;
-    -boot) boot="-boot once=d -cdrom $2"; shift;;
-    *) disk="$1"
+    -m) MEM="-m $2"; shift;;
+    -cdrom) CDROM="-cdrom $2"; shift;;
+    -vga) VGA="-vga $2"; shift;;
+    -boot) BOOT="-boot once=d -cdrom $2"; shift;;
+    -display) DISPLAY="-display $2"; shift;;
+    *) DISK="-hda $1"
   esac
   shift
 done
 
-if [[ $boot ]]; then
-  qemu-system-x86_64  -accel kvm -cpu host -smp 2 -display sdl,gl=on $vga $memory $boot $disk
-else
-  if [[ $(ip a | grep -o br0:) != "br0:" ]]; then
-    nmcli c up br0
-  fi
-  qemu-system-x86_64  -accel kvm -cpu host -smp 2 $display $vga $net $memory $cdrom $disk
-fi
+qemu-system-x86_64 -accel kvm -cpu host -smp 2 $DISPLAY $VGA $NET $MEM $BOOT $CDROM $DISK
