36b3ccada2f49b7cded5f343a888f442205c6e7c
hivernal <geryaevna@mail.ru>
2023-10-21 11:34:14 +0700

    change qemu script

--- .config/scripts/run.sh
+++ .config/scripts/run.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+NUMBER=0
+
 SPICE="-display spice-app,gl=on -device virtio-serial -chardev spicevmc,id=vdagent,debug=0,name=vdagent -device virtserialport,chardev=vdagent,name=com.redhat.spice.0"
-TAP="-nic tap,helper=/usr/lib/qemu/qemu-bridge-helper,mac=52:54:00:12:34:56"
-SOCKET="-nic socket,mcast=230.0.0.1:1234,mac=52:54:00:12:34:56"
-USER="-nic user,smb=/home/nikita,mac=52:54:00:12:34:56"
+TAP="-nic tap,helper=/usr/lib/qemu/qemu-bridge-helper,mac=52:54:00:12:34:$(($NUMBER*3+56))"
+SOCKET="-nic socket,mcast=230.0.0.1:1234,mac=52:54:00:12:34:$(($NUMBER*3+57))"
+USER="-nic user,smb=/home/nikita,mac=52:54:00:12:34:$(($NUMBER*3+58))"
 
 DISPLAY=$SPICE
 VGA="-vga qxl"
-NET=$TAP
+NET=$USER
 MEM="-m 1G"
 CDROM=
 BOOT=
-DISK="-hda openvpn.qcow2"
+DISK="-hda clear.qcow2"
 
 while [[ -n $1 ]]; do
   case $1 in
