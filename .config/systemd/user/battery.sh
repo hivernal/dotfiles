7cbc29a879ec223328e0eda6fbcdaef11f00ed29
hivernal <hivernal@example.com>
2023-11-29 00:44:03 +0700

    changed script for battery notification

--- .config/systemd/user/battery.sh
+++ .config/systemd/user/battery.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-BAT_CAP=$(< /sys/class/power_supply/BAT0/capacity)
-BAT_LOW=20
+BAT0_CAP=$(< /sys/class/power_supply/BAT0/capacity)
+BAT0_STATUS=$(< /sys/class/power_supply/BAT0/status)
+BAT0_LOW=20
 
-if [[ $BAT_CAP -le $BAT_LOW ]]; then
-  dunstify -i /usr/share/icons/Qogir-dark/16/panel/battery-020.svg "Battery is low"
+if (( $BAT0_CAP <= $BAT0_LOW )) && [[ $BAT0_STATUS != "Charging" ]]; then
+  dunstify -t 20000 -i /usr/share/icons/Qogir-dark/16/panel/battery-020.svg "Battery is low!"
 fi
