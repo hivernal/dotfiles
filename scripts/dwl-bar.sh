6a84e9da1a54358fffd9511bd174a0c41344c7c1
Geryaev Nikita <geryaevna@mail.ru>
2026-03-01 10:56:53 +0700

    change dwl scripts

--- scripts/dwl-bar.sh
+++ scripts/dwl-bar.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 while true; do
-  kb_layout="$(cat /tmp/dwl-keymap 2> /dev/null)"
-  if [[ "${kb_layout}" == "English (US)" ]]; then
-    kb_layout="en"
-  elif [[ "${kb_layout}" == "Russian" ]]; then
-    kb_layout="ru"
-  else
-    kb_layout="n/a"
-  fi
+#   kb_layout="$(cat /tmp/dwl-keymap 2> /dev/null)"
+#   if [[ "${kb_layout}" == "English (US)" ]]; then
+#     kb_layout="en"
+#   elif [[ "${kb_layout}" == "Russian" ]]; then
+#     kb_layout="ru"
+#   else
+#     kb_layout="n/a"
+#   fi
   volume="$(volume.sh)"
   date="$(date +%H:%M:%S)"
   ram_usage="$(free -h | awk '(NR==2) {printf $3}')"
-  echo " KEY ${kb_layout} | VOL ${volume} | MEM ${ram_usage} | ${date}"
-  sleep 0.5
+#   echo " KEY ${kb_layout} | VOL ${volume} | MEM ${ram_usage} | ${date}"
+  echo " VOL ${volume} | MEM ${ram_usage} | ${date}"
+  sleep 1
 done
