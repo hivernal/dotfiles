88c11e2ae73a24cd8df693a5a575c0c76438c141
Geryaev Nikita <geryaevna@mail.ru>
2026-04-28 12:12:37 +0700

    change brightness.sh script

--- scripts/brightness.sh
+++ scripts/brightness.sh
@@ -4,9 +4,10 @@ SCRIPT_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ICON="${SCRIPT_DIR}/icons/icons8-sun.svg"
 
 if [[ "$1" == "up" ]] then
-  doas xbacklight -inc 5
+  brightness="$(brightnessctl s +5% | grep -Eo "[0-9]{1,3}%")"
 elif [[ "$1" == "down" ]]; then
-  doas xbacklight -dec 5
+  brightness="$(brightnessctl s 5%- | grep -Eo "[0-9]{1,3}%")"
 fi
-brightness=$(doas xbacklight -get)
+brightness=${brightness%\%}
+
 dunstify -t 2000 -i "${ICON}" -h string:x-canonical-private-synchronous:brightness "${brightness}%" -h int:value:${brightness}
